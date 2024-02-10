521b6f101158150aaa53d8446f97476eb058e3b6
Gavande Parag Dattatray <130244009+Parag-Gavande@users.noreply.github.com>
2024-02-10 18:09:54 +0530

    Time: 3 ms (77.07%) | Memory: 12 MB (33.62%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int n=arr.size();
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++){    // Time Complexity = O(n)
             if(arr[i]<=k){
                 k=k+1;
             }
@@ -11,3 +11,7 @@ public:
         return k;
     }
 };
+
+//---------------------------OR------------------
+// Time Complexity = O(log n)
+
