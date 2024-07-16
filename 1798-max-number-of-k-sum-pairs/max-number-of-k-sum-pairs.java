4e293d73f7c2afddd03472e0766131a480b30a7e
Gavande Parag Dattatray <130244009+Parag-Gavande@users.noreply.github.com>
2024-07-17 03:08:42 +0530

    Time: 35 ms (35.19%) | Memory: 54.7 MB (97.65%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -1,23 +1,44 @@
-// Two Pointer approach 
-
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Arrays.sort(nums);
-        int s=0;
-        int e=nums.length-1;
-        int count=0;
 
-        while(s<e){
-            if(nums[s]+nums[e]>k){
-                e--;
-            }else if(nums[s]+nums[e]<k){
-                s++;
-            }else{
-                count++;
-                s++;
-                e--;
+        // Using HashMap
+        int cnt = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            int c = k - num;
+            if (map.containsKey(c)) {
+                cnt++;
+                if(map.get(c)==1){
+                    map.remove(c);
+                }
+                else{
+                map.put(c, map.get(c) - 1);
+                }
+            } else {
+                map.put(num, map.getOrDefault(num, 0) + 1);
             }
         }
-        return count;
+        return cnt;
+
+        // Two Pointer approach 
+
+        // Arrays.sort(nums);
+        // int s=0;
+        // int e=nums.length-1;
+        // int count=0;
+
+        // while(s<e){
+        //     if(nums[s]+nums[e]>k){
+        //         e--;
+        //     }else if(nums[s]+nums[e]<k){
+        //         s++;
+        //     }else{
+        //         count++;
+        //         s++;
+        //         e--;
+        //     }
+        // }
+        // return count;
     }
 }
